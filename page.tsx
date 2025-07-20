4646404e36be6292255b0201ee806ed4508f29ab
LogistSolutions <logisttransport2018@gmail.com>
2025-07-20 18:18:21 +0200

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -3,6 +3,3 @@ import AssetpreisKalkulator from "./components/AssetpreisKalkulator";
 export default function Home() {
   return <AssetpreisKalkulator />;
 }
-
-
-git push -u origin codespace-organic-yodel-v654xpww5qj6cx55w
