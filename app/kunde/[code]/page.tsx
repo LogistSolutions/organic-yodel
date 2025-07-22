f2b6501761fe38739899ef8dc5fd06787c83aed3
LogistSolutions <logisttransport2018@gmail.com>
2025-07-22 00:05:11 +0000

    update 1.0

--- app/kunde/[code]/page.tsx
+++ app/kunde/[code]/page.tsx
@@ -2,7 +2,7 @@ import AssetpreisKalkulator from "../../components/AssetpreisKalkulator";
 import config from "../../data/config.json";
 import { notFound } from "next/navigation";
 
-export default async function KundePage({ params }) {
+export default async function KundePage({ params }: { params: { code: string } }) {
   const kundencode = params.code;
   // @ts-ignore: config-Kunden ist any
   const kundeConfig = config.kunden[kundencode];
