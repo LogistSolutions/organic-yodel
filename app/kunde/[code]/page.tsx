df1b059989830429ac3a518cc64fff46208a7f26
LogistSolutions <logisttransport2018@gmail.com>
2025-07-23 00:17:13 +0200

    Update page.tsx

--- app/kunde/[code]/page.tsx
+++ app/kunde/[code]/page.tsx
@@ -2,9 +2,8 @@ import AssetpreisKalkulator from '../../components/AssetpreisKalkulator';
 import config from "../../data/config.json";
 import { notFound } from "next/navigation";
 
-type Params = { params: { code: string } };
-
-export default function KundePage({ params }: Params) {
+// Typisierung direkt in der Funktionssignatur:
+export default function KundePage({ params }: { params: { code: string } }) {
   const kundeConfig = config.kunden[params.code];
   if (!kundeConfig) return notFound();
 
