ef2435dbadc8a46c2393ac4cba22c8fa489db526
LogistSolutions <logisttransport2018@gmail.com>
2025-07-26 09:16:59 +0000

    v9

--- app/api/html-to-pdf/route.ts
+++ app/api/html-to-pdf/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { chromium } from 'playwright';
+import { chromium } from 'playwright-chromium';
 
 export async function POST(req: NextRequest) {
   try {
